d80cda36bb8f8751d06ef2367522fcbc8a72df17
Will Franklin <will@willpf.co.uk>
2023-04-26 11:58:11 +0100

    Check version compatibility before updating

--- update.sh
+++ update.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+# Check if API_VERSION and FRONTEND_VERSION are compatible
+# TODO: check ROUTER_VERSION too
+API_VERSION=$(cat API_VERSION)
+FRONTEND_VERSION=$(cat FRONTEND_VERSION)
+
+echo "API_VERSION: $API_VERSION"
+echo "FREONTEND_VERSION: $FRONTEND_VERSION"
+
+part="major"
+if [ $(./semver.sh get major ${API_VERSION:1}) -eq 0 ]; then
+    part="minor"
+fi
+
+if [ $(./semver.sh get $part ${API_VERSION:1}) -ne $(./semver.sh get $part ${FRONTEND_VERSION:1}) ]; then
+    echo "API_VERSION and FRONTEND_VERSION are not compatible, ignoring"
+    exit
+fi
+
 cat docker-compose.tmpl.yml |
-    sed "s/API_VERSION/$(cat API_VERSION)/" |
-    sed "s/FRONTEND_VERSION/$(cat FRONTEND_VERSION)/" |
+    sed "s/API_VERSION/$API_VERSION/" |
+    sed "s/FRONTEND_VERSION/$FRONTEND_VERSION/" |
     sed "s/ROUTER_VERSION/$(cat ROUTER_VERSION)/" > docker-compose.yml
